ccef8320630fc425219e58027d29cbceada7c020
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2023-05-03 21:41:22 -0300

    Criando queue e routingkey

--- pagamento/src/main/java/com/microservices/pagamento/config/MessageConfig.java
+++ pagamento/src/main/java/com/microservices/pagamento/config/MessageConfig.java
@@ -1,7 +1,11 @@
 package com.microservices.pagamento.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,11 +16,27 @@ import org.springframework.context.annotation.Configuration;
 public class MessageConfig {
 
 	@Value("${crud.rabbitmq.exchange}")
-	public String exchange;
+	public String exchangeName;
+	
+	@Value("${crud.rabbitmq.queue}")
+	public String queueName;
+	
+	@Value("${crud.rabbitmq.routingkey}")
+	public String routingKeyName;
 	
 	@Bean
 	public Exchange declareExchange() {
-		return ExchangeBuilder.directExchange(exchange).durable(true).build();
+		return ExchangeBuilder.directExchange(exchangeName).durable(true).build();
+	}
+	
+	@Bean
+	public Queue declareQueue() {
+		return QueueBuilder.durable(queueName).build();
+	}
+	
+	@Bean
+	public Binding declareBinding() {
+		return BindingBuilder.bind(declareQueue()).to(declareExchange()).with(routingKeyName).noargs();
 	}
 	
 	@Bean
